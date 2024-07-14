388127f3703385a94782580c14174cb59aa0c899
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-15 00:40:50 +0500

    install nodemailer and setup nodemailer

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,24 @@
-const sendEmail = (req, res) => {
-  res.send("Send email func");
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "ericka.mclaughlin49@ethereal.email",
+      pass: "hw8sRadHQT3Rs7YnWG",
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: "'Coding Addict' <codingaddict@gmail.com>",
+    to: "bar@example.com",
+    subject: "Hello",
+    html: "<h2>Sending Emails with Node.js</h2>",
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
